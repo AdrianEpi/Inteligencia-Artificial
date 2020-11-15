12c9adbd7eea6e68ba9610682617ad2a28fcbae5
Adrian Epifanio Rodriguez <alu0101158280@ull.edu.es>
2020-11-15 14:17:23 +0000

    version final

--- Práctica 1/html/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['inteligencia_2dartificial_0',['Inteligencia-Artificial',['../md__r_e_a_d_m_e.html',1,'']]]
-];
--- Práctica 1/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['inteligencia_2dartificial_1',['Inteligencia-Artificial',['../md__r_e_a_d_m_e.html',1,'']]]
-];
